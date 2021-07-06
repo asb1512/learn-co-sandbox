9e0bba7c49384079a402948d9ced90a61799abca
Andrew Bourgeois <asb1512@gmail.com>
2021-07-06 14:49:32 +0000

    Automatically backed up by Learn

---
+++ my_new_directory/variables.rb
@@ -0,0 +1,11 @@
+first_number = 7
+second_number = 14
+
+sum = first_number + second_number
+
+puts sum
+
+puts "Peepee Poopoo"
+
+current_president = "Barack Obama"
+puts "In 2016, the president was #{current_president}."
